0ba75d8f7ed3eb68787c95ce426b498cdf006c9c
victor hanyk <viktor.hanyk@gmail.com>
2018-09-03 15:57:40 +0000

    initial2

--- js/index.js
+++ js/index.js
@@ -3,8 +3,6 @@ let a = parseInt(prompt('Enter a'));
 let b = parseInt(prompt('Enter b'));
 let c = parseInt(prompt('Enter c'));
 
-
-
 var x1, x2;
 
 function solution(a, b, c){
@@ -12,9 +10,6 @@ var d=Math.pow(2, b)-4*a*c
 if (d<0){
     x1=x2='the roots of the quadratic equation are complex';
 }
-    else if(d==0){
-        x1=x2=(-b)/2*a;
-    }
         else{
             x1=-(b+Math.sqrt(d))/2*a;
             x2=-(b-Math.sqrt(d))/2*a;
