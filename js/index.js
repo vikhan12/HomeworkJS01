3185895f15d8445456fe84e645975794e7b8cde6
vikhan12 <42488209+vikhan12@users.noreply.github.com>
2018-09-03 19:34:31 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
-
 let a = parseInt(prompt('Enter a'));
 let b = parseInt(prompt('Enter b'));
 let c = parseInt(prompt('Enter c'));
 
-
 function solution(a, b, c, x1, x2){
-let d=Math.pow(2, b)-4*a*c
+let d=Math.pow(b, 2)-4*a*c
 if (d<0){
     x1=x2='the roots of the quadratic equation are complex';
 }
         else{
-            x1=-(b+Math.sqrt(d))/2*a;
-            x2=-(b-Math.sqrt(d))/2*a;
+            x1=(-b+Math.sqrt(d))/(2*a);
+            x2=(-b-Math.sqrt(d))/(2*a);
         }
     return[x1, x2]
 }
